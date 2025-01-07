07a8800c66efbcba6e5193402f5d070e6cc11c52
Marco Oosthuizen <marco.oosthuizen@entelect.co.za>
2025-01-07 08:49:24 +0200

    Update mock data in main to Mario characters

--- main.py
+++ main.py
@@ -24,23 +24,22 @@ if __name__ == '__main__':
     show_output = True
     setup_output(show_output)
 
-    month = 9
-    year = 2024
+    month = 1
+    year = 2025
     schedule_parameters = ScheduleParameters(
         schedule_start_date=datetime(year, month, 2),
         schedule_end_date=datetime(year, month, 27),
-        devs=['William', 'Mokgali', 'Marco', 'Natasha', 'Juan', 'Vuyani'],
+        devs=['Mario', 'Luigi', 'Toad', 'Yoshi'],
         dev_leave_days={
-            'William': [(datetime(year, month, 2), datetime(year, month, 13))],
-            'Mokgali': [(datetime(year, month, 4), datetime(year, month, 10))],
-            'Marco': [datetime(year, month, 23)],
-            'Natasha': [datetime(year, month, 23)],
-            'Vuyani': [(datetime(year, month, 25), datetime(year, month, 27))]
+            'Mario': [(datetime(year, month, 2), datetime(year, month, 13))],
+            'Luigi': [(datetime(year, month, 4), datetime(year, month, 10))],
+            'Toad': [datetime(year, month, 23)],
+            'Yoshi': [datetime(year, month, 23)],
         },
-        dimensions=2,
+        dimensions=1,
     )
 
-    # Seed 3 does pretty good
+    # Change the seed to explore different results
     ga_parameters = GAParameters(
         seed=3,
         generations=200,
